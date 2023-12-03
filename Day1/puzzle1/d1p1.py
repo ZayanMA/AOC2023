d48784dd279e1012d84dbfed3ee6809a4da34c89
zayanMA <za1g22@soton.ac.uk>
2023-12-03 15:33:28 +0000

    Completed code for first puzzle

---
+++ Day1/puzzle1/d1p1.py
@@ -0,0 +1,23 @@
+import re
+
+def getCalibrationValues(input):
+    f = open(input, 'r')
+    values = []
+    line = f.readline()
+    while line:
+        line = line.rstrip()
+        num1 = re.search(r"\d", line).group(0)
+        num2 = re.findall(r'\d', line)[-1]
+        num = int(num1 + num2)
+        values.append(num)
+        line = f.readline()
+    f.close()
+    #print(values)
+    print(sum(values))
+
+def main():
+    getCalibrationValues("input.txt")
+
+if __name__ == "__main__":
+    main()
+
